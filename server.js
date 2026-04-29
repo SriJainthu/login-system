87b455c72d9785d6c09a7c6d9977fe674e1b372c
Sri Jainthu <kannappanlatha00@gmail.com>
2026-04-29 14:17:16 +0530

    fix9

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
 }));
-app.options('*', cors());
+app.options('/*', cors());
 app.use(bodyParser.json());
 app.use(express.static("public"));
 // Add these BEFORE your routes
